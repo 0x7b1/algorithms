1b42b20f952a09d6cb90f26764882461441f8b8e
carlos <vonzo55@gmail.com>
2020-10-10 18:40:09 +0000

    pushing from codespaces

--- std/list.go
+++ std/list.go
@@ -10,6 +10,7 @@ func main() {
 	intList.PushBack(11)
 	intList.PushBack(10)
 	intList.PushBack(9)
+	intList.PushBack(13)
 
 	for element := intList.Front(); element != nil; element = element.Next() {
 		fmt.Println(element.Value.(int))
