a2a4af9710d550291970dac76b06ed9de1c26dfa
JCR <vonzo55@gmail.com>
2020-10-13 12:41:42 +0300

    1 added

---
+++ leetcode/0001.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	d := make(map[int]int)
+
+	for i, num := range nums {
+		if idx, ok := d[target-num]; ok {
+			return []int{idx, i}
+		} else {
+			d[num] = i
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+}
