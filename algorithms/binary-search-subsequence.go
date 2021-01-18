96451018a0f68fd1491765a4e5dd06a7987557d1
JCR <vonzo55@gmail.com>
2021-01-18 12:06:50 +0200

    added binary search substring

---
+++ algorithms/binary-search-subsequence.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func leftBound(arr []int, tar int) int {
+	lo, hi := 0, len(arr)
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if tar > arr[mid] {
+			lo = mid + 1
+		} else {
+			hi = mid
+		}
+	}
+	return lo
+}
+
+// Time: O(MlogN)
+func isSubsequence(s, t string) bool {
+	// Store indices of each character in a dictionary
+	index := make([][]int, 256)
+
+	for i, c := range t {
+		if index[c] == nil {
+			index[c] = make([]int, 0)
+		}
+
+		index[c] = append(index[c], i)
+	}
+
+	// look for subsequence
+	j := 0
+	for _, c := range s {
+		if index[c] == nil {
+			return false
+		}
+
+		pos := leftBound(index[c], j)
+		if pos == len(index[c]) {
+			return false
+		}
+		
+		j = index[c][pos] + 1
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(isSubsequence("abc", "cacbhbc"))
+	fmt.Println(isSubsequence("axc", "cacbhbc"))
+}
