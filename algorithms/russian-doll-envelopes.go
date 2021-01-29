0bc76be2948ccbc97db4a39ab76c689b8e21be9f
JCR <vonzo55@gmail.com>
2021-01-29 16:24:36 +0200

    completed envelope alg

--- algorithms/russian-doll-envelopes.go
+++ algorithms/russian-doll-envelopes.go
@@ -5,30 +5,58 @@ import (
 	"sort"
 )
 
-type HeightWidthPair []int
+type Pairs [][]int
 
-func (h HeightWidthPair) Len() int {
-
+func (a Pairs) Len() int {
+	return len(a)
 }
 
-func (h HeightWidthPair) Less(i, j int) bool {
-
+func (a Pairs) Less(i, j int) bool {
+	return a[i][0] < a[j][0]
 }
 
-func (h * HeightWidthPair) Swap(i, j int)  {
-
+func (a Pairs) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
 }
 
-func maxEnvelopes(envelopes []HeightWidthPair) []HeightWidthPair {
-	//n := len(envelopes)
-	sort.Sort(envelopes)
+func maxEnvelopes(envelopes [][]int) int {
+	n := len(envelopes)
+	sort.Sort(Pairs(envelopes))
+	fmt.Println(envelopes)
+	height := make([]int, n)
 
+	for i := 0; i < n; i++ {
+		height[i] = envelopes[i][1]
+	}
+
+	return lengthOfLongestIncreasingSubsequence(height)
+}
+
+func lengthOfLongestIncreasingSubsequence(nums []int) (piles int) {
+	top := make([]int, len(nums))
+
+	for _, num := range nums {
+		left, right := 0, piles // TODO: revise pilar's value
+		for left < right {
+			mid := (left + right) / 2
+			if top[mid] >= num {
+				right = mid
+			} else {
+				left = mid + 1
+			}
+
+			if left == num {
+				piles++
+			}
+
+			top[left] = num
+		}
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(maxEnvelopes([]HeightWidthPair{
-		{2, 20},
-		{1, 10},
-		{4, 40},
-	}))
+	arr := [][]int{{5, 4}, {6, 4}, {6, 7}, {2, 3}}
+	fmt.Println(maxEnvelopes(arr))
 }
