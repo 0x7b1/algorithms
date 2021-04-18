bfe8efa009335fce2a5715463824d5b298be1447
JCR <vonzo55@gmail.com>
2021-04-18 17:38:18 +0300

    revamp 0872

--- leetcode/0872.go
+++ leetcode/0872.go
@@ -11,7 +11,55 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func travel(node *TreeNode, c chan int) {
+	if node.Left == nil && node.Right == nil {
+		c <- node.Val
+		return
+	}
+
+	if node.Left != nil {
+		travel(node.Left, c)
+	}
+
+	if node.Right != nil {
+		travel(node.Right, c)
+	}
+}
+
+func leafChan(root *TreeNode) chan int {
+	c := make(chan int)
+
+	go func() {
+		travel(root, c)
+		close(c)
+	}()
+
+	return c
+}
+
 func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+	ch1 := leafChan(root1)
+	ch2 := leafChan(root2)
+
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+
+		if ok1 && ok2 {
+			if v1 == v2 {
+				continue
+			} else {
+				return false
+			}
+		} else if !ok1 && !ok2 {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+func leafSimilar2(root1 *TreeNode, root2 *TreeNode) bool {
 	return reflect.DeepEqual(printTree(root1), printTree(root2))
 }
 
@@ -37,35 +85,32 @@ func printTree(t *TreeNode) []int {
 	return res
 }
 
-func printTree2(t *TreeNode) {
-	if t != nil {
-		printTree(t.Left)
-		if t.Left == nil && t.Right == nil {
-			fmt.Println(t.Val)
-		}
-		printTree(t.Right)
-	}
-}
-
 func main() {
-	tree1 := &TreeNode{
-		3,
-		&TreeNode{
-			5,
+	tree1 := &TreeNode{3,
+		&TreeNode{5,
 			&TreeNode{6, nil, nil},
-			&TreeNode{
-				2,
+			&TreeNode{2,
 				&TreeNode{7, nil, nil},
 				&TreeNode{4, nil, nil},
 			},
 		},
-		&TreeNode{
-			1,
+		&TreeNode{1,
 			&TreeNode{9, nil, nil},
 			&TreeNode{8, nil, nil},
 		},
 	}
 
-	//tree2 := &TreeNode{}
-	fmt.Println(leafSimilar(tree1, tree1))
+	tree2 := &TreeNode{3,
+		&TreeNode{5,
+			&TreeNode{6, nil, nil},
+			&TreeNode{7, nil, nil}},
+		&TreeNode{1,
+			&TreeNode{4, nil, nil},
+			&TreeNode{2,
+				&TreeNode{9, nil, nil},
+				&TreeNode{8, nil, nil}}},
+	}
+
+	fmt.Println(leafSimilar2(tree1, tree2))
+	fmt.Println(leafSimilar(tree1, tree2))
 }
